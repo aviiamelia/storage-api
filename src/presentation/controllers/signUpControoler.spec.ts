221437490775500faecf74a630ff3a879e9dab3a
rafael <rafael.zoe26@gmail.com>
2024-02-01 21:57:10 -0300

    test: signcontroller

--- src/presentation/controllers/signUpControoler.spec.ts
+++ src/presentation/controllers/signUpControoler.spec.ts
@@ -1,16 +1,44 @@
+import { ZodIssue } from "zod";
+import { MissingParamError } from "../error/missingParamsError";
 import { SignUpController } from "./signUp";
+import { UserTypeSchema } from "../protocols/user.schema";
+import { ValuesValidator } from "../protocols/valuesValidator";
+
+const makeSut = () => {
+  class UserBodyValidatorStub implements ValuesValidator {
+    isValid(values: UserTypeSchema) {
+      if (values) {
+        return true;
+      }
+      return false;
+    }
+  }
+  return new SignUpController(new UserBodyValidatorStub());
+};
 
 describe("signUpController", () => {
-  test("Should return 400 if no username is Provided", () => {
-    const sut = new SignUpController();
+  test("Should return 400 if no proper values are provided", () => {
+    const sut = makeSut();
     const httpRequest = {
       body: {
         username: "any name",
-        email: "any@mail.com",
         password: "any_password",
+        email: "rafael@email.com",
       },
     };
+    const errorMock = [
+      {
+        code: "invalid_type",
+        expected: "boolean",
+        received: "undefined",
+        path: ["isAdmin"],
+        message: "Required",
+      },
+    ];
     const httpResponse = sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
+    console.log(httpResponse.body.issues);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError(errorMock as ZodIssue[])
+    );
   });
 });
