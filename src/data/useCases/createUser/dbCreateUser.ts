4845ddc1d610b6b2711204fa4c85b2d10fe59566
rafael <rafael.zoe26@gmail.com>
2024-02-14 23:55:36 -0300

    feat: ensure encrytpter is called when creating a user

---
+++ src/data/useCases/createUser/dbCreateUser.ts
@@ -0,0 +1,24 @@
+import {
+  createUser,
+  CreateUserModel,
+} from "../../../domain/useCases/createUser";
+import { UserModel } from "../../../domain/models/userModel";
+import { Encrypter } from "../../protocols/encrypter";
+
+export class DbCreateUser implements createUser {
+  private readonly encrypter;
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter;
+  }
+
+  async create(user: CreateUserModel): Promise<UserModel> {
+    const data = {
+      ...user,
+      id: "1",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.encrypter.encrypt(user.password);
+    return new Promise((resolve) => resolve(data));
+  }
+}
